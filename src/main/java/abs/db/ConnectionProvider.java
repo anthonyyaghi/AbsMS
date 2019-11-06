d81ff4be7d737156a7b5380e0a3b8c9a9082aa32
anthonyyaghi <anthonyyaghi@outlook.com>
2019-11-06 16:55:29 +0200

    connect pannel

--- src/main/java/abs/db/ConnectionProvider.java
+++ src/main/java/abs/db/ConnectionProvider.java
@@ -5,6 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionProvider {
+    private static String host = "";
+    private static String username = "";
+    private static String db = "";
+    private static String pass = "";
+
+
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://localhost/abs?user=admin&password=pass");
     }
